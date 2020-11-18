773608f10bacb78d20937a947d0fa51390519aa3
Raghav25012003 <56781829+Raghav25012003@users.noreply.github.com>
2020-11-18 19:55:21 +0530

    through this student can loged into his/her account

--- main.cpp
+++ main.cpp
@@ -1,22 +1,57 @@
 #include<iostream>
+#include<map>
 using namespace std;
+map<int,float> roll_borrow; //Map that contain borrow as value and roll no as key
 class student{
-	string name;
-	int ID,borrow_amount;
+	int ID;
+	float borrow_amount;
 	public:
 		student(){
 			borrow_amount=0;
 		}
-		void getInput(){
-			cout<<"Enter Name :";cin>>name;cout<<endl;
+		void newAccount(){
 			cout<<"Enter Your Roll No. :";cin>>ID;cout<<endl;
+			if(newUser(ID)){
+				roll_borrow.insert(pair<int,int> (ID,borrow_amount));
+				logedIn(ID);
+			}else{
+				cout<<"This ID is already registed!"<<endl;
+			}
 		}
 		void show_borrow(){
 			cout<<borrow_amount<<endl;
 		}
+		bool newUser(int key){
+			if (roll_borrow.find(key) == roll_borrow.end()) 
+                return true; 
+  
+              return false; 
+		}
+		void checkUser(){
+             cout<<"Enter you ID"<<endl;
+			 int tempID;
+			 cin>>tempID;
+			 if(newUser(tempID)){
+				cout<<"No account exist with this ID"<<endl;
+			}else{
+				logedIn(tempID);
+			}
+
+		}
+		void logedIn(int ID){
+			cout<<"Welcome "<<ID<<endl;
+		}
 };
 int main(){
-	student s;//this is for master branch and this is new one
-    s.getInput();
+	student s;
+	int count=0;
+    cout<<"Already have an account ? y/n";
+	char ans;
+	cin>>ans;
+	if(ans=='n' || ans=='N'){
+		s.newAccount();
+	}else{
+		s.checkUser();
+	}
 	return 0;
 }
