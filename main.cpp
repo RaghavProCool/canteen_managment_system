5f61a2b3dc51bdb4873b0207a027003f43279740
Raghav25012003 <56781829+Raghav25012003@users.noreply.github.com>
2020-11-18 15:22:57 +0530

    just checking

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class student{
+	string name;
+	int ID,borrow_amount;
+	public:
+		student(){
+			borrow_amount=0;
+		}
+		void getInput(){
+			cout<<"Enter Name :";cin>>name;cout<<endl;
+			cout<<"Enter Your Roll No. :";cin>>ID;cout<<endl;
+		}
+		void show_borrow(){
+			cout<<borrow_amount<<endl;
+		}
+};
+int main(){
+	
+	return 0;
+}
